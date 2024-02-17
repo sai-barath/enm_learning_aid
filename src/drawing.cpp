88177d2d0d4bd4ceca28962630d989f0d2b2b135
Sai Sudarshan Barath <saisudarshanb@gmail.com>
2024-02-17 17:08:16 -0600

    Add source file for drawing

---
+++ src/drawing.cpp
@@ -0,0 +1,8 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "R3Vectors.h"
+#include "drawing.h"
+
+void Artist::drawvector(sf::RenderWindow& win, const vectorR3& start, const vectorR3& vec) {
+    /*implement here*/
+}
