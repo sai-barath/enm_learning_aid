16e153af3b4201ffde407ca7fd276d3f2251ea49
Sai Sudarshan Barath <saisudarshanb@gmail.com>
2024-02-17 17:06:33 -0600

    Get drawing started

---
+++ src/drawing.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "R3Vectors.h"
+
+class Artist {
+    //Draws vectors
+    void drawvector(sf::RenderWindow& win, const vectorR3& start, const vectorR3& vec);
+    //Check both vectors have z component 9
+};
