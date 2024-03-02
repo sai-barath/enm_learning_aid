8a22ba4821ebfb7452872d4dbeee03a319280de1
Sai Sudarshan Barath <saisudarshanb@gmail.com>
2024-03-02 17:14:56 -0600

    Write first test

--- test/testing.cpp
+++ test/testing.cpp
@@ -2,3 +2,9 @@
 #include "R3Vectors.h"
 #include "point_charge.h"
 
+TEST(vectorR3_tests, Bvector_setup) {
+    vectorR3 vec(4.0, 3.0, 1.0);
+    EXPECT_EQ(vec.x_component, 4.0);
+    EXPECT_EQ(vec.y_component, 3.0);
+    EXPECT_EQ(vec.z_component, 1.0);
+}
