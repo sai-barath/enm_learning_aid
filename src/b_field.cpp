e8aa2c9c19a2ef57dc695bacc63c8c232369fd68
amp1130 <andrey.pridgen@gmail.com>
2024-03-02 16:27:23 -0600

    Added documentation

--- src/b_field.cpp
+++ src/b_field.cpp
@@ -10,16 +10,7 @@ B_Field_Vector::B_Field_Vector(const vectorR3 &v, double curr) {
     this->direction = v;
 }
 
-/**
- * Find the magnetic field at the given position
- *
- * @param x, y, z the coordinates at which to compute the field
- * @return The magnetic field at the given position
- */
 vectorR3 B_Field_Vector::compute_B_field(double x, double y, double z) {
-    /*Given the x, y, and z components of the point, calculates the B-field vector at
-     that point by ampere's law*/
-
     vectorR3 pos = vectorR3(x, y, z);
     vectorR3 R = direction.vector_project(pos) - pos; // Vector pointing from the position to the wire, normal to the wire
 
