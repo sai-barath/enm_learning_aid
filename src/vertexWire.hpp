e961bf6ffb65763a19d8d9b3349fad6b68154726
Sai Sudarshan Barath <saisudarshanb@gmail.com>
2024-08-26 20:33:13 -0500

    finish b-field calculation

--- src/vertexWire.hpp
+++ src/vertexWire.hpp
@@ -44,9 +44,22 @@ vectorR3 wireOfVertices::bField(const vectorR3& pos) const {
    * Compute the component of the b-Field due to the segment of wire starting at vertex i; 
    * if last vertex, the wire goes back to the first
    **/
-  double constant = (MU0 * this->current) / (4 * PI);
+  double currentConstant = (MU0 * this->current) / (4 * PI);
   for(int i = 0; i < numSides; i++) {
-    vectorR3 dir = vertices[(i + 1) % numSides] - vertices[i];
+    vectorR3 currVertex = vertices[i];
+    vectorR3 nextVertex = vertices[(i + 1) % numSides];
+    vectorR3 dir = nextVertex - currVertex;
+    // What follows is the solution of the Biot-Savart integral
+    double integralConstant = (pos.yComponent * dir.xComponent) - (currVertex.yComponent * dir.xComponent) + (currVertex.xComponent * dir.yComponent) - (pos.xComponent * dir.yComponent); 
+    double a = -dir.xComponent, b = pos.xComponent - currVertex.xComponent, c = -dir.yComponent, d = pos.yComponent - pos.yComponent;
+    double bcAd = 1.0 / (((b * c) - (a * d)) * ((b * c) - (a * d)));
+    double combinedConstant = integralConstant * currentConstant * bcAd;
+    double numeratorOne = (a * a) + (a * b) + ((c + d) * c);
+    double denomOne = std::sqrt((a + b) * (a + b) + (c + d) * (c + d));
+    double numeratorTwo = (a * b) + (c * d);
+    double denomTwo = std::sqrt((b * b) + (d * d));
+    double zComp = combinedConstant * ((numeratorOne / denomOne) - (numeratorTwo / denomTwo));
+    B.zComponent += zComp;
   }
   return B;
 }
