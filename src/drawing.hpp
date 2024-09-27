48c7b29dcfaa0de130e14b807fc1452aec694d29
Sai Sudarshan Barath <saisudarshanb@gmail.com>
2024-09-27 13:54:37 -0500

    recover changes lost by rebase

--- src/drawing.hpp
+++ src/drawing.hpp
@@ -11,7 +11,6 @@
 #include <vector>
 
 namespace draw {
-<<<<<<< HEAD
     /**
      * Draw a 2D vector on the screen
      *
@@ -27,119 +26,6 @@ namespace draw {
             vec *= (80 / vec.magnitude());
         } else if(vec.magSquared() < 100) {
             vec *= (10 / vec.magnitude());
-=======
-    void drawVector(sf::RenderWindow &win, const vectorR3& begin, const vectorR3& vect);
-    void drawCharge(sf::RenderWindow& win, pointCharge& pc);
-    void intoOut(sf::RenderWindow& win, const vectorR3& where, const vectorR3& vec);
-    void drawElecField(sf::RenderWindow& win, std::vector<pointCharge>& charges);
-    void drawBField(sf::RenderWindow& win, const longThinWire& wir);
-    void drawVertexWire(sf::RenderWindow& win, wireOfVertices& wir, std::vector<std::vector<double>>& cache);
-};
-
-
-void draw::drawVector(sf::RenderWindow &win, const vectorR3& begin, const vectorR3& vect) {
-    int triangleSize = 10;
-    vectorR3 start(begin.xComponent, win.getSize().y - begin.yComponent, 0);
-    vectorR3 vec(vect.xComponent, -vect.yComponent, 0);
-    if(vec.magSquared() > 6400) {
-        vec *= (80 / vec.magnitude());
-    } else if(vec.magSquared() < 100) {
-        vec *= (10 / vec.magnitude());
-    }
-    sf::Vertex line[] =
-    {
-        sf::Vertex(sf::Vector2f(start.xComponent, start.yComponent), sf::Color::Black),
-        sf::Vertex(sf::Vector2f(start.xComponent + vec.xComponent, start.yComponent + vec.yComponent), sf::Color::Black)
-    };
-
-    // Rotating the arrow
-    double angle = 0.0;
-    if(vec.xComponent != 0.0) {
-        angle = atan(vec.yComponent / vec.xComponent) * (180 / PI);
-    } else if(vec.xComponent == 0 && vec.yComponent < 0) {
-        angle = 270;
-    } else if(vec.xComponent == 0 && vec.yComponent > 0) {
-        angle = 90;
-    }
-    if(vec.xComponent < 0.0) {
-        angle += 180.0;
-    }
-    sf::CircleShape triangle(triangleSize, 3);
-    triangle.setFillColor(sf::Color::Black);
-    triangle.setOrigin(triangleSize, triangleSize);
-    triangle.setPosition(start.xComponent + vec.xComponent, start.yComponent + vec.yComponent);
-    triangle.rotate(angle + 90);
-    win.draw(line,2 , sf::Lines);
-    win.draw(triangle);
-}
-
-void draw::drawCharge(sf::RenderWindow& win, pointCharge& pc) {
-    sf::CircleShape charge(8);
-    pc.pos.xComponent = std::floor(pc.pos.xComponent / 100) * 100;
-    pc.pos.yComponent = std::floor(pc.pos.yComponent / 100) * 100;
-    charge.setOrigin(8, 8);
-    charge.setPosition(pc.pos.xComponent, win.getSize().y - pc.pos.yComponent);
-    if(pc.charge <= 0.0) {
-        charge.setFillColor(sf::Color::Blue); 
-        win.draw(charge);
-    } else {
-        charge.setFillColor(sf::Color::Red);
-        win.draw(charge);
-    } 
-   
-}
-
-void draw::intoOut(sf::RenderWindow& win, const vectorR3& where, const vectorR3& vec) {
-    double zComp = std::abs(vec.zComponent);
-    if(zComp > 48.0) {
-        zComp = 48.0;
-    } else if(zComp < 5.0 && zComp != 0) {
-        zComp = 5.0;
-    }
-    sf::CircleShape sym(zComp);
-    sym.setOrigin(zComp, zComp);
-    sym.setFillColor(sf::Color::White);
-    sym.setPosition(where.xComponent, win.getSize().y - where.yComponent);
-    sym.setOutlineThickness(2);
-    sym.setOutlineColor(sf::Color::Black);
-    win.draw(sym);
-    if(vec.zComponent < 0.0) {
-        double lineLen = zComp / SQRT2;
-        sf::Vertex line1[] = {
-            sf::Vertex(sf::Vector2f(where.xComponent - lineLen, win.getSize().y - where.yComponent + lineLen), sf::Color::Black),
-            sf::Vertex(sf::Vector2f(where.xComponent + lineLen, win.getSize().y - where.yComponent - lineLen), sf::Color::Black)
-        };
-        sf::Vertex line2[] = {
-            sf::Vertex(sf::Vector2f(where.xComponent + lineLen, win.getSize().y - where.yComponent + lineLen), sf::Color::Black),
-            sf::Vertex(sf::Vector2f(where.xComponent - lineLen, win.getSize().y - where.yComponent - lineLen), sf::Color::Black)
-        };
-        win.draw(line1, 2, sf::Lines);
-        win.draw(line2, 2, sf::Lines);
-    } else {
-        sf::CircleShape dot(zComp / 4.0);
-        dot.setOrigin(zComp / 4.0, zComp / 4.0);
-        dot.setPosition(where.xComponent, win.getSize().y - where.yComponent);
-        dot.setFillColor(sf::Color::Black);
-        win.draw(dot);
-    }
-}
-
-void draw::drawElecField(sf::RenderWindow& win, std::vector<pointCharge>& charges) {
-    int numc = charges.size();
-    for(int i = 0; i < numc; i++) {
-        draw::drawCharge(win, charges[i]);
-    }
-    int numx = (win.getSize().x / 100) + 1;
-    int numy = (win.getSize().y / 100) + 1;
-    vectorR3** board = new vectorR3*[numx];
-    bool** isChargePresent = new bool*[numx];
-    for(int i = 0; i < numx; i++) {
-        board[i] = new vectorR3[numy];
-        isChargePresent[i] = new bool[numy];
-        for(int j = 0; j < numy; j++) {
-            board[i][j] = vectorR3(i * 100, j * 100, 0);
-            isChargePresent[i][j] = 0;
->>>>>>> 1568540 (Register click event and draw wire)
         }
         sf::Vertex line[] =
         {
@@ -189,7 +75,6 @@ void draw::drawElecField(sf::RenderWindow& win, std::vector<pointCharge>& charge
     
     }
 
-<<<<<<< HEAD
     /**
      * Draw a vector with only a z-component
      *
@@ -266,20 +151,6 @@ void draw::drawElecField(sf::RenderWindow& win, std::vector<pointCharge>& charge
                 }
                 if(!isChargePresent[i][j]) {
                     draw::drawVector(win, board[i][j], efieldatpos);
-=======
-void draw::drawVertexWire(sf::RenderWindow& win, wireOfVertices& wir, std::vector<std::vector<double>>& cache) {
-    if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
-        sf::Vector2i position = sf::Mouse::getPosition();
-        wir.addVertex(position.x, win.getSize().y - position.y);
-        std::cout << vectorR3(position.x, win.getSize().y - position.y, 0.0) << std::endl;
-        if(wir.vertices.size() >= 3) {
-            for(int i = 0; i <= win.getSize().x; i += win.getSize().x / 100) {
-                for(int j = 0; j <= win.getSize().y; j += win.getSize().y / 100) {
-                    vectorR3 pos(i * 100, j * 100, 0.0);
-                    vectorR3 bField = wir.bField(pos);
-                    cache[i][j] = bField.zComponent;
-                    draw::intoOut(win, pos, bField);
->>>>>>> 60a44db (fix: no longer loop pixel by pixel)
                 }
             }
         }
@@ -344,5 +215,31 @@ void draw::drawVertexWire(sf::RenderWindow& win, wireOfVertices& wir, std::vecto
             }
         } 
         win.draw(wire, 2, sf::Lines);
+    }
+
+    void drawVertexWire(sf::RenderWindow& win, wireOfVertices& wir, std::vector<std::vector<double>>& cache) {
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+            sf::Vector2i position = sf::Mouse::getPosition();
+            wir.addVertex(position.x, win.getSize().y - position.y);
+            std::cout << vectorR3(position.x, win.getSize().y - position.y, 0.0) << std::endl;
+            if(wir.vertices.size() >= 3) {
+                for(int i = 0; i <= win.getSize().x; i += win.getSize().x / 100) {
+                    for(int j = 0; j <= win.getSize().y; j += win.getSize().y / 100) {
+                        vectorR3 pos(i * 100, j * 100, 0.0);
+                        vectorR3 bField = wir.bField(pos);
+                        cache[i][j] = bField.zComponent;
+                        draw::intoOut(win, pos, bField);
+                    }
+                }
+            }
+        }
+        if(wir.vertices.size() >= 3) {
+            sf::Vertex wire[wir.vertices.size() + 1];
+            for(int i = 0; i < wir.vertices.size(); i++) {
+                wire[i] = sf::Vertex(sf::Vector2f(wir.vertices[i].xComponent, win.getSize().y - wir.vertices[i].yComponent), sf::Color::Black);
+            }
+            wire[wir.vertices.size()] = wire[0];
+            win.draw(wire, wir.vertices.size() + 1, sf::Lines);
+        }
     }
 };
