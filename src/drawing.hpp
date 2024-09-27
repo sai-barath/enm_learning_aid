ead2f5b61d1c14b3dff9ae36c548749310f1de03
Sai Sudarshan Barath <saisudarshanb@gmail.com>
2024-09-27 14:29:45 -0500

    fix: bring test file up to date

--- src/drawing.hpp
+++ src/drawing.hpp
@@ -5,19 +5,11 @@
 #include "drawing.hpp"
 #include "pointCharge.hpp"
 #include "bField.hpp"
-#include "vertexWire.hpp"
 #include <cmath>
 #include <iostream>
 #include <vector>
 
 namespace draw {
-    /**
-     * Draw a 2D vector on the screen
-     *
-     * @param win The window to draw on
-     * @param begin start point
-     * @param vect actual vector
-     */
     void drawVector(sf::RenderWindow &win, const vectorR3& begin, const vectorR3& vect) {
         int triangleSize = 10;
         vectorR3 start(begin.xComponent, win.getSize().y - begin.yComponent, 0);
@@ -53,12 +45,7 @@ namespace draw {
         win.draw(line,2 , sf::Lines);
         win.draw(triangle);
     }
-    /**
-     * Draw a small circle representing a charge on the screen
-     *
-     * @param win The window to draw on
-     * @param pc The point charge
-     */
+
     void drawCharge(sf::RenderWindow& win, pointCharge& pc) {
         sf::CircleShape charge(8);
         pc.pos.xComponent = std::floor(pc.pos.xComponent / 100) * 100;
@@ -75,13 +62,6 @@ namespace draw {
     
     }
 
-    /**
-     * Draw a vector with only a z-component
-     *
-     * @param win The window to draw on
-     * @param where start point
-     * @param vec actual vector
-     */
     void intoOut(sf::RenderWindow& win, const vectorR3& where, const vectorR3& vec) {
         double zComp = std::abs(vec.zComponent);
         if(zComp > 48.0) {
@@ -117,12 +97,6 @@ namespace draw {
         }
     }
 
-    /**
-     * Draw a set of charges and their associated electric fields
-     *
-     * @param win The window to draw on
-     * @param charges  an std::vector consisting of the charges
-     */
     void drawElecField(sf::RenderWindow& win, std::vector<pointCharge>& charges) {
         int numc = charges.size();
         for(int i = 0; i < numc; i++) {
@@ -156,23 +130,16 @@ namespace draw {
         }
     }
 
-    /**
-     * Draw an arbitrarily long wire and its magnetic field
-     *
-     * @param win The window to draw on
-     * @param wir The wire
-     */
     void drawBField(sf::RenderWindow& win, const longThinWire& wir) {
         double windowSlopeFactor = win.getSize().y / win.getSize().x;
         double slope = wir.direction.yComponent / wir.direction.xComponent;
+        double angle = atan(slope) * (180 / PI);
         double maxX = -1.0;
         double maxY = -1.0;
-        //If our wire is steeper than the window, we have to truncate it to avoid drawing out of bounds
-        if (slope > windowSlopeFactor) {
+        if(slope > windowSlopeFactor) { // steep
             maxX = win.getSize().y / slope;
             maxY = 0;
         } else {
-            //Otheriwse just draw until it hits the right edge of the window
             maxX = win.getSize().x;
             maxY = slope * win.getSize().x;
         }
@@ -180,29 +147,13 @@ namespace draw {
             sf::Vertex(sf::Vector2f(0, win.getSize().y), sf::Color::Black),
             sf::Vertex(sf::Vector2f(maxX, maxY), sf::Color::Black)
         };
-        //Calculate angle *after* we know the final wire vector
-        double angle = atan2(wire[1].position.y - wire[0].position.y, wire[1].position.x - wire[0].position.x) * (180 / PI);
-
         double triangleSize = 10.0;
         for(double xPos = 0.0; xPos < maxX; xPos += (maxX / 5)) {
-
-            //Temporary fix for #33; calculate "center of mass" or average position, then rotate
-            //Let's move this into a helper/utility class later.
-            //We also aren't drawing enough triangles when slope < 1, is this intended?
-            //---
             sf::CircleShape triangle(triangleSize, 3);
-            sf::Vector2f newTriangleOrigin;
-            for (int i = 0; i < 3; i++) {
-                sf::Vector2f trianglePoint = triangle.getPoint(i);
-                newTriangleOrigin.x += trianglePoint.x;
-                newTriangleOrigin.y += trianglePoint.y;
-            }
-            triangle.setOrigin(newTriangleOrigin.x / 3, newTriangleOrigin.y / 3);
-            //---
-            
             triangle.setFillColor(sf::Color::Red);
-            triangle.rotate(angle + 90);
+            triangle.setOrigin(triangleSize, triangleSize);
             triangle.setPosition(xPos, win.getSize().y - (slope * xPos));
+            triangle.rotate(angle - 90);
             win.draw(triangle);
         }
         // Don't need to use SFML's coord system here
@@ -215,31 +166,5 @@ namespace draw {
             }
         } 
         win.draw(wire, 2, sf::Lines);
-    }
-
-    void drawVertexWire(sf::RenderWindow& win, wireOfVertices& wir, std::vector<std::vector<double>>& cache) {
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
-            sf::Vector2i position = sf::Mouse::getPosition();
-            wir.addVertex(position.x, win.getSize().y - position.y);
-            std::cout << vectorR3(position.x, win.getSize().y - position.y, 0.0) << std::endl;
-            if(wir.vertices.size() >= 3) {
-                for(int i = 0; i <= win.getSize().x; i += win.getSize().x / 100) {
-                    for(int j = 0; j <= win.getSize().y; j += win.getSize().y / 100) {
-                        vectorR3 pos(i * 100, j * 100, 0.0);
-                        vectorR3 bField = wir.bField(pos);
-                        cache[i][j] = bField.zComponent;
-                        draw::intoOut(win, pos, bField);
-                    }
-                }
-            }
-        }
-        if(wir.vertices.size() >= 3) {
-            sf::Vertex wire[wir.vertices.size() + 1];
-            for(int i = 0; i < wir.vertices.size(); i++) {
-                wire[i] = sf::Vertex(sf::Vector2f(wir.vertices[i].xComponent, win.getSize().y - wir.vertices[i].yComponent), sf::Color::Black);
-            }
-            wire[wir.vertices.size()] = wire[0];
-            win.draw(wire, wir.vertices.size() + 1, sf::Lines);
-        }
     }
 };
--- test/testing.cpp
+++ test/testing.cpp
@@ -1,110 +1,115 @@
 #include <gtest/gtest.h>
-#include "R3Vectors.h"
-#include "point_charge.h"
-#include "b_field.h"
+#include "vectorR3.hpp"
+#include "pointCharge.hpp"
+#include "bField.hpp"
+#include "vertexWire.hpp"
 #include <cmath>
 
 TEST(vectorR3_tests, Bvector_setup) {
     vectorR3 vec(4.0, 3.0, 1.0);
-    EXPECT_EQ(vec.x_component, 4.0);
-    EXPECT_EQ(vec.y_component, 3.0);
-    EXPECT_EQ(vec.z_component, 1.0);
+    EXPECT_EQ(vec.xComponent, 4.0);
+    EXPECT_EQ(vec.yComponent, 3.0);
+    EXPECT_EQ(vec.zComponent, 1.0);
     vectorR3 vec2;
-    EXPECT_EQ(vec2.x_component, 0.0);
-    EXPECT_EQ(vec2.y_component, 0.0);
-    EXPECT_EQ(vec2.z_component, 0.0);
+    EXPECT_EQ(vec2.xComponent, 0.0);
+    EXPECT_EQ(vec2.yComponent, 0.0);
+    EXPECT_EQ(vec2.zComponent, 0.0);
 }
 
 TEST(vectorR3_tests, cross_product) {
     vectorR3 vec1(4.0, 3.0, 1.0);
     vectorR3 vec2(-7.0, 3.0, 2.0);
     vectorR3 vec3 = vec1.cross(vec2);
-    EXPECT_EQ(vec3.x_component, 3.0);
-    EXPECT_EQ(vec3.y_component, -15.0);
-    EXPECT_EQ(vec3.z_component, 33.0);
+    EXPECT_EQ(vec3.xComponent, 3.0);
+    EXPECT_EQ(vec3.yComponent, -15.0);
+    EXPECT_EQ(vec3.zComponent, 33.0);
     vectorR3 vec4(1.0, 2.0, 3.0);
     vectorR3 vec5(-1.0, -2.0, -3.0);
     vectorR3 vec6 = vec4.cross(vec5);
-    EXPECT_EQ(vec6.x_component, 0.0);
-    EXPECT_EQ(vec6.y_component, 0.0);
-    EXPECT_EQ(vec6.z_component, 0.0);
+    EXPECT_EQ(vec6.xComponent, 0.0);
+    EXPECT_EQ(vec6.yComponent, 0.0);
+    EXPECT_EQ(vec6.zComponent, 0.0);
     vectorR3 vec7 = vec2.cross(vec1);
-    EXPECT_EQ(vec7.x_component, -3.0);
-    EXPECT_EQ(vec7.y_component, 15.0);
-    EXPECT_EQ(vec7.z_component, -33.0);
+    EXPECT_EQ(vec7.xComponent, -3.0);
+    EXPECT_EQ(vec7.yComponent, 15.0);
+    EXPECT_EQ(vec7.zComponent, -33.0);
     vectorR3 vec8;
     vectorR3 vec9;
     vectorR3 vec10 = vec8.cross(vec9);
-    EXPECT_EQ(vec10.x_component, 0.0);
-    EXPECT_EQ(vec10.y_component, 0.0);
-    EXPECT_EQ(vec10.z_component, 0.0);
+    EXPECT_EQ(vec10.xComponent, 0.0);
+    EXPECT_EQ(vec10.yComponent, 0.0);
+    EXPECT_EQ(vec10.zComponent, 0.0);
 }
 
-TEST(pointcharge, init) {
+TEST(pointCharge, init) {
     vectorR3 pos(-3.0, 7.3, 20.1);
-    PointCharge c1(pos, 3.3);
-    EXPECT_EQ(c1.pos.x_component, -3.0);
-    EXPECT_EQ(c1.pos.y_component, 7.3);
-    EXPECT_EQ(c1.pos.z_component, 20.1);
+    pointCharge c1(pos, 3.3);
+    EXPECT_EQ(c1.pos.xComponent, -3.0);
+    EXPECT_EQ(c1.pos.yComponent, 7.3);
+    EXPECT_EQ(c1.pos.zComponent, 20.1);
     EXPECT_EQ(c1.charge, 3.3);
 }
 
 
 TEST(efieldtest, efield_1comp) {
-    PointCharge c1(0.0, 0.0, 0.0, 1.0);
-    PointCharge c2(0.0, 0.0, 0.0, -1.0);
+    pointCharge c1(0.0, 0.0, 0.0, 1.0);
+    pointCharge c2(0.0, 0.0, 0.0, -1.0);
     double ke = 1.0 / (4.0 * PI * EP0);
     vectorR3 loc(1.0, 0.0 , 0.0);
     vectorR3 e1 = c1.efield(loc);
-    EXPECT_EQ(e1.x_component, ke);
-    EXPECT_EQ(e1.y_component, 0.0);
-    EXPECT_EQ(e1.z_component, 0.0);
+    EXPECT_EQ(e1.xComponent, ke);
+    EXPECT_EQ(e1.yComponent, 0.0);
+    EXPECT_EQ(e1.zComponent, 0.0);
     vectorR3 e2 = c2.efield(loc);
-    EXPECT_EQ(e2.x_component, -ke);
-    EXPECT_EQ(e2.y_component, 0.0);
-    EXPECT_EQ(e2.z_component, 0.0);
+    EXPECT_EQ(e2.xComponent, -ke);
+    EXPECT_EQ(e2.yComponent, 0.0);
+    EXPECT_EQ(e2.zComponent, 0.0);
 }
 
 TEST(efieldtest, efield_multcomp) {
-    PointCharge c1(0.0, 0.0, 0.0, 1.0);
-    PointCharge c2(0.0, 0.0, 0.0, -1.0);
+    pointCharge c1(0.0, 0.0, 0.0, 1.0);
+    pointCharge c2(0.0, 0.0, 0.0, -1.0);
     double ke = 1.0 / (4.0 * PI * EP0);
     vectorR3 e3 = c2.efield(vectorR3(1.0, 1.0, 1.0));
     vectorR3 e4 = c1.efield(vectorR3(1.0, 1.0, 1.0));
-    double mag = e3.getMagnitude();
-    double mag2 = e4.getMagnitude();
+    double mag = e3.magnitude();
+    double mag2 = e4.magnitude();
     EXPECT_EQ(mag, ke / 3);
     EXPECT_EQ(mag2, ke / 3);
-    EXPECT_EQ(e3.x_component < 0, 1);
-    EXPECT_EQ(e3.y_component < 0, 1);
-    EXPECT_EQ(e3.z_component < 0, 1);
-    EXPECT_EQ(e4.x_component > 0, 1);
-    EXPECT_EQ(e4.y_component > 0, 1);
-    EXPECT_EQ(e4.z_component > 0, 1);
+    EXPECT_EQ(e3.xComponent < 0, 1);
+    EXPECT_EQ(e3.yComponent < 0, 1);
+    EXPECT_EQ(e3.zComponent < 0, 1);
+    EXPECT_EQ(e4.xComponent > 0, 1);
+    EXPECT_EQ(e4.yComponent > 0, 1);
+    EXPECT_EQ(e4.zComponent > 0, 1);
 }
 
 TEST(bfieldtest, one_dimension) {
-    Long_Thin_Wire wire(1, 0, 100); //100 amps along x axis
+    longThinWire wire(1, 0, 100); //100 amps along x axis
 
     //1 mm away
-    vectorR3 b1 = wire.compute_B_field(0, 0, 0.001);
-    vectorR3 b2 = wire.compute_B_field(0, 0.001, 0);
+    vectorR3 b1 = wire.computeBField(vectorR3(0, 0, 0.001));
+    vectorR3 b2 = wire.computeBField(vectorR3(0, 0.001, 0));
     vectorR3 B1(0, -0.02, 0);
     vectorR3 B2(0, 0, 0.02);
 
-    EXPECT_EQ(B1, b1);
-    EXPECT_EQ(B2, b2);
+    EXPECT_NEAR(B1.xComponent, b1.xComponent, 0.00001);
+    EXPECT_NEAR(B1.yComponent, b1.yComponent, 0.00001);
+    EXPECT_NEAR(B1.zComponent, b1.zComponent, 0.00001);
+    EXPECT_NEAR(B2.xComponent, b2.xComponent, 0.00001);
+    EXPECT_NEAR(B2.yComponent, b2.yComponent, 0.00001);
+    EXPECT_NEAR(B2.zComponent, b2.zComponent, 0.00001);
 }
 
 TEST(bfieldtest, two_dimensions) {
-    Long_Thin_Wire wire(1, 0, 100); // 100 amps along x axis
+    longThinWire wire(1, 0, 100); // 100 amps along x axis
 
     // 5 mm away
-    vectorR3 b3 = wire.compute_B_field(0, 0.003, 0.004);
-    vectorR3 B3(0, -0.0032, 0.0024);
+    vectorR3 b3 = wire.computeBField(vectorR3(0, 0.003, 0.004));
+    vectorR3 B3(vectorR3(0, -0.0032, 0.0024));
 
     //account for overflow
-    EXPECT_NEAR(B3.x_component, b3.x_component, 0.0000001);
-    EXPECT_NEAR(B3.y_component, b3.y_component, 0.0000001);
-    EXPECT_NEAR(B3.z_component, b3.z_component, 0.0000001);
+    EXPECT_NEAR(B3.xComponent, b3.xComponent, 0.0000001);
+    EXPECT_NEAR(B3.yComponent, b3.yComponent, 0.0000001);
+    EXPECT_NEAR(B3.zComponent, b3.zComponent, 0.0000001);
 }
